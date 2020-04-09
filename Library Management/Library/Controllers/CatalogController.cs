8917c317f204087bdd1ce6671f6aa241ed8bfdab
Vlad <vollos@abv.bg>
2020-04-09 18:09:01 +0300

    Complex Library Management added Data

---
+++ Library Management/Library/Controllers/CatalogController.cs
@@ -0,0 +1,85 @@
+﻿using Library.ViewModels.Catalog;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly ILibraryAsset assets;
+
+        public CatalogController(ILibraryAsset assets)
+        {
+            this.assets = assets;
+        }
+
+        public IActionResult Index()
+        {
+            var assetModels = assets.GetAll();
+
+            var model = new List<AssetIndexListingModel>();
+
+            foreach (var item in assetModels)
+            {
+                var modelToAdd = new AssetIndexListingModel
+                {
+                    Id = item.Id,
+                    ImageUrl = item.ImageUrl,
+                    AuthorOrDirector = assets.GetAuthorOrDirector(item.Id),
+                    DeweyCallNumber = assets.GetDeweyIndex(item.Id),
+                    Title = item.Title,
+                    Type = assets.GetType(item.Id)
+                };
+
+                model.Add(modelToAdd);
+
+            }
+
+            //var listingResult = assetModels
+            //    .Select(result => new AssetIndexListingModel
+            //    {
+            //        Id = result.Id,
+            //        ImageUrl = result.ImageUrl,
+            //        AuthorOrDirector = assets.GetAuthorOrDirector(result.Id),
+            //        DeweyCallNumber = assets.GetDeweyIndex(result.Id),
+            //        Title = result.Title,
+            //        Type = assets.GetType(result.Id)
+
+            //    });
+
+            //var model = new AssetIndexModel()
+            //{
+            //    Assets = listingResult
+            //};
+
+            return View(model);
+
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var asset = assets.GetById(id);
+
+            var model = new AssetDetailModel
+            {
+                AssetId = id,
+                Title = asset.Title,
+                Year = asset.Year,
+                Cost = asset.Cost,
+                Status = asset.Status.Name,
+                ImageUrl = asset.ImageUrl,
+                AuthorOrDirector = assets.GetAuthorOrDirector(id),
+                CurrentLocation = assets.GetLocation(id).Name,
+                DeweyCallNumber = assets.GetDeweyIndex(id),
+                ISBN = assets.GetIsbn(id)
+            };
+
+            return View(model);
+
+        }
+    }
+}
---
+++ Library Management/LibraryData/ICheckout.cs
@@ -0,0 +1,25 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData
+{
+    public interface ICheckout
+    {
+        IEnumerable<Checkout> GetAll();
+        Checkout GetById(int checkoutId);
+        void Add(Checkout newCheckout);  
+        void CheckoutItem(int assetId, int libraryCardId);
+        void CheckInItem(int assetId, int libraryCardId);
+        IEnumerable<CheckoutHistory> GetCheckoutHistory(int id);
+
+        void PlaceHold(int assetId, int libraryCardId);
+        string GetCurrentHoldPatronName(int id);
+        DateTime GetCurrentHoldPlaced(int id);
+        IEnumerable<Hold> GetCurrentHolds(int id);
+        Checkout GetLatestCheckout(int assetid);
+        void MarkLost(int assetid);
+        void MarkFound(int assetid);
+    }
+}
